3189a499df9669942a89967ffe5c60a469442e17
chrishaynes21 <chris.haynes2112@gmail.com>
2019-04-18 12:52:13 -0600

    Add button color change on click

--- app/static/src/Loader.js
+++ app/static/src/Loader.js
@@ -2,22 +2,47 @@ import React from 'react';
 import {Button, Row, Col,Container} from 'reactstrap';
 
 class Loader extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeButton: null}
+    }
+
+    handleDecadeChange() {
+        this.setState({activeButton: 'decade'})
+    }
+
+    handleBodyChange() {
+        this.setState({activeButton: 'bodystyle'})
+    }
+
+    handlePriceChange() {
+        this.setState({activeButton: 'price'})
+    }
+
     render() {
         return (
             <div>
                 <Container>
                     <Row>
                         <Col>
-                            <Button size={'lg'} block>Decade</Button>
+                            <Button color={this.state.activeButton === 'decade' ? 'primary' : 'secondary'} size={'lg'}
+                                    block onClick={this.handleDecadeChange.bind(this)}>
+                                Decade
+                            </Button>
                         </Col>
                         <Col>
-                            <Button size={'lg'} block>Body Style</Button>
+                            <Button color={this.state.activeButton === 'bodystyle' ? 'primary' : 'secondary'} size={'lg'}
+                                     block onClick={this.handleBodyChange.bind(this)}>
+                                Body Style
+                            </Button>
                         </Col>
                         <Col>
-                            <Button size={'lg'} block>Price</Button>
+                            <Button color={this.state.activeButton === 'price' ? 'primary' : 'secondary'} size={'lg'}
+                                     block onClick={this.handlePriceChange.bind(this)}>
+                                Price
+                            </Button>
                         </Col>
                     </Row>
-
                 </Container>
             </div>
         )
